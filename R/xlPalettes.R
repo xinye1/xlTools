50faf605fece650826e5d57209d2c7b457047241
XI373146 <xinye.li4@wipro.com>
2017-05-30 11:20:09 +0100

    Completed xlPalettes.R functions

--- R/xlPalettes.R
+++ R/xlPalettes.R
@@ -1,44 +1,9 @@
-#' Colour Palettes
-#'
-#' These functions return various colour palettes I have collected over time from
-#' places including companies I have worked for.
-#'
-#' @details To do: how to use.
-#'
-#' Currently the available palettes are as follow:
-#'
-#'
-#'
-#' @param cols character or numeric vector for the names or the indices of colours
-#' in the palette specified by \code{palette}.
-#' @param palette a string, the colour palette name.
-#' @param alpha a number between \code{[0, 1]} for the opacity, 0 being opac.
-#' @param ... parameters passed to either \code{\link{rgb}} (for \code{xlRgbtoHex})
-#' or \code{\link{img}} (for \code{xlColPalDisp}).
-#'
-#' @references
-#' Wipro colours are taken from
-#' the \href{https://wipro365-my.sharepoint.com/personal/scshekar_wipro_com/Documents/Analytics%20-%20Overview%20-%202017.pptx?web=1}{
-#' internal template}
-#'
-#'
-#'
-#' @examples
-#' xlColPal()
-#' xlColPal(c(1, 2, 3))
-#' xlColPal(c('red', 'cyan', 'grey'))
-#' xlColPal(rep(c(1, 2), 3)) # repeats are allowed
-#'
-#' \dontrun{
-#' # What not to do:
-#' xlColPal(c(1, 2, 3), palette = 'wrong') # error, wrong palette name
-#' xlColPal(c(356, 6))                     # error, as 356 is outside of 1:10
-#' xlColPal(c(1, 'cyan', 'grey'))          # error, mixed cols element types
-#' xlColPal(c('1'))                        # error, numeric should not be quoted
-#' }
-
-
-
+#' RGB to Hex colour
+#' Convert RGB colours to Hexadecimal format
+#' @keywords internal
+#' @param rgb_list a list of RGB numerical vectors.
+#' @param max_col_val numerical, the maximum value for the RGB colours, default 255
+#' @param col_names character vector, the names of the colours, default the names of the input list
 xlRgbtoHex <- function(rgb_list, max_col_val = 255, col_names = names(rgb_list), ...) {
 
   rgb_df <- as.data.frame(t(data.frame(
@@ -49,7 +14,57 @@ xlRgbtoHex <- function(rgb_list, max_col_val = 255, col_names = names(rgb_list),
 
 }
 
+
+#' Colour Palettes
+#'
+#' These functions return hexadecimal colours from the
+#' various colour palettes I have collected over time from
+#' places including companies I have worked for.
+#'
+#' @details To do: how to use.
+#'
+#' Currently the available palettes are as follow:
+#' \itemize{
+#'  \item \code{"ee"}: EE colours, from the official internal branding document.
+#'  Internally the colours are in the list \code{ee_hex_cols} in hexadecimal format.
+#'  \item \code{"wipro"}: Wipro colours, from the initial template (see Reference below).
+#'  Internally the colours are in the list \code{wipro_rgb_cols} in RGB format.
+#' }
+#'
+#'
+#' @param cols character or numeric vector for the names or the indices of colours
+#' in the palette specified by \code{palette}.
+#' @param palette a string, the colour palette name.
+#' @param alpha a number between \code{[0, 1]} for the opacity, 0 being opac.
+#' @param ... parameters passed to either \code{\link{rgb}} for \code{\link{xlColPal}},
+#' or \code{\link{img}} for \code{\link{xlColPalDisp}}.
+#'
+#' @references
+#' Wipro colours are taken from
+#' the \href{https://wipro365-my.sharepoint.com/personal/scshekar_wipro_com/Documents/Analytics%20-%20Overview%20-%202017.pptx?web=1}{
+#' internal template}
+#'
+#' @rdname xlPalettes
 #' @export
+#'
+#'
+#' @examples
+#' xlColPal()
+#' xlColPal(c(1, 2, 3))
+#' xlColPal(c('red', 'cyan', 'grey'))
+#' xlColPal(rep(c(1, 2), 3)) # repeats are allowed
+#' xlColPalDisp()
+#' xlColPalDisp(c(1, 2, 3), 'ee')
+#'
+#' \dontrun{
+#' # What not to do:
+#' xlColPal(c(1, 2, 3), palette = 'wrong') # error, wrong palette name
+#' xlColPal(c(356, 6))                     # error, as 356 is outside of 1:10
+#' xlColPal(c(1, 'cyan', 'grey'))          # error, mixed cols element types
+#' xlColPal(c('1'))                        # error, numeric should not be quoted
+#' }
+
+
 
 xlColPal <- function(cols = NULL, palette = "wipro", alpha = 1) {
 
@@ -90,7 +105,7 @@ xlColPal <- function(cols = NULL, palette = "wipro", alpha = 1) {
 }
 
 
-
+#' @rdname xlPalettes
 #' @export
 
 
